9780ad52d1ae0437e89bd2da7809811bf096e8ac
PaleSky <126697791+PaleSky1@users.noreply.github.com>
2024-11-22 23:03:13 +0200

    Add files via upload

---
+++ ContractClaimSystemMvc/ContractClaimSystemMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContractClaimSystemMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
